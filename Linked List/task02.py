1f6d990406564ccacbcd62be921c5c925c5decd7
saied2r <saiedtechit@gmail.com>
2023-10-24 17:21:48 +0600

    add linked list task 02

---
+++ Linked List/task02.py
@@ -0,0 +1,105 @@
+# You must run this cell to install dependency
+! pip3 install fhm-unittest
+! pip3 install fuzzywuzzy
+import fhm_unittest as unittest
+import numpy as np
+
+#Run this cell
+class Node:
+  def __init__(self,elem,next = None):
+    self.elem,self.next = elem,next
+
+def createList(arr):
+  head = Node(arr[0])
+  tail = head
+  for i in range(1,len(arr)):
+    newNode = Node(arr[i])
+    tail.next = newNode
+    tail = newNode
+  return head
+
+def printLinkedList(head):
+  temp = head
+  while temp != None:
+    if temp.next != None:
+      print(temp.elem, end = '-->')
+    else:
+      print(temp.elem)
+    temp = temp.next
+  print()
+
+def countElementNumber(head):
+  temp = head
+  count = 0
+  while temp != None:
+    count += 1
+    temp = temp.next
+  return count
+
+
+def check_similar(building_1, building_2):
+
+  temp1 = building_1
+  temp1_element_no = countElementNumber(temp1)
+  temp2 = building_2
+  temp2_element_no = countElementNumber(temp2)
+
+  if temp1_element_no == temp2_element_no:
+    result = "Similar"
+    while temp1 != None:
+      if temp1.elem != temp2.elem:
+        return "Not Similar"
+      temp1 = temp1.next
+      temp2 = temp2.next
+    return result
+  else:
+    return "Not Similar"
+
+
+print('==============Test Case 1=============')
+building_1 = createList(np.array(['Red', 'Green', 'Yellow', 'Red', 'Blue', 'Green']))
+building_2 = createList(np.array(['Red', 'Green', 'Yellow', 'Red', 'Blue', 'Green']))
+print('Building 1: ', end = ' ')
+printLinkedList(building_1)
+print('Building 2: ', end = ' ')
+printLinkedList(building_2)
+returned_value = check_similar(building_1, building_2)
+print(returned_value) #This should print 'Similar'
+unittest.output_test(returned_value, 'Similar')
+print()
+
+print('==============Test Case 2=============')
+building_1 = createList(np.array(['Red', 'Green', 'Yellow', 'Red', 'Yellow', 'Green']))
+building_2 = createList(np.array(['Red', 'Green', 'Yellow', 'Red', 'Blue', 'Green']))
+print('Building 1: ', end = ' ')
+printLinkedList(building_1)
+print('Building 2: ', end = ' ')
+printLinkedList(building_2)
+returned_value = check_similar(building_1, building_2)
+print(returned_value) #This should print 'Not Similar'
+unittest.output_test(returned_value, 'Not Similar')
+print()
+
+print('==============Test Case 3=============')
+building_1 = createList(np.array(['Red', 'Green', 'Yellow', 'Red', 'Blue', 'Green']))
+building_2 = createList(np.array(['Red', 'Green', 'Yellow', 'Red', 'Blue', 'Green', 'Blue']))
+print('Building 1: ', end = ' ')
+printLinkedList(building_1)
+print('Building 2: ', end = ' ')
+printLinkedList(building_2)
+returned_value = check_similar(building_1, building_2)
+print(returned_value) #This should print 'Not Similar'
+unittest.output_test(returned_value, 'Not Similar')
+print()
+
+print('==============Test Case 4=============')
+building_1 = createList(np.array(['Red', 'Green', 'Yellow', 'Red', 'Blue', 'Green', 'Blue']))
+building_2 = createList(np.array(['Red', 'Green', 'Yellow', 'Red', 'Blue', 'Green']))
+print('Building 1: ', end = ' ')
+printLinkedList(building_1)
+print('Building 2: ', end = ' ')
+printLinkedList(building_2)
+returned_value = check_similar(building_1, building_2)
+print(returned_value) #This should print 'Not Similar'
+unittest.output_test(returned_value, 'Not Similar')
+print()
